47047d7338027149eb21f1707e32afeb93019465
Leo Taydakov <leo.taydakov@arrow.com>
2018-07-20 08:21:50 -0600

    Style checkout summary section

--- src/components/CheckoutSummary/CheckoutSummary.js
+++ src/components/CheckoutSummary/CheckoutSummary.js
@@ -5,13 +5,19 @@ export default class CheckoutSummary extends Component {
   render() {
     return (
       <div className="CheckoutSummary">
-        <span className="subtotal-label">Subtotal</span>
-        <span className="subtotal-value">{this.props.summaryData.subtotal}</span>
-        <span className="pickup-savings-label">Pickup Savings</span>
-        <span className="pickup-savings-value">{this.props.summaryData.pickupSavings}</span>
-        <span className="est-taxes-fees-label">Est. taxes & fees</span>
-        <span className="est-taxes-fees-value">{this.props.summaryData.estTaxesFees}</span>
-        <span className="zipcode-label">(Based on {this.props.summaryData.zipCode})</span>
+        <div className="subtotal-container">
+          <span className="subtotal-label">Subtotal</span>
+          <span className="subtotal-value">{this.props.summaryData.subtotal}</span>
+        </div>
+        <div className="pickup-savings-container">
+          <span className="pickup-savings-label">Pickup Savings</span>
+          <span className="pickup-savings-value">{this.props.summaryData.pickupSavings}</span>
+        </div>
+        <div className="est-taxes-fees-container">
+          <span className="est-taxes-fees-label">Est. taxes & fees</span>
+          <span className="est-taxes-fees-value">{this.props.summaryData.estTaxesFees}</span>
+        </div>
+        <div className="zipcode-label">(Based on {this.props.summaryData.zipCode})</div>
       </div>
     );
   }
